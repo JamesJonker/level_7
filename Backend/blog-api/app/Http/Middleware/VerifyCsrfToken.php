b1d537ea51a9b3a4d603f0ba9ea0f9e467345e11
James <jonker.james@gmail.com>
2024-09-04 09:11:55 +0200

    removing hardcoded paths to get paths from env file

--- Backend/blog-api/app/Http/Middleware/VerifyCsrfToken.php
+++ Backend/blog-api/app/Http/Middleware/VerifyCsrfToken.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
+
 {
 
     /**
@@ -12,17 +13,37 @@ class VerifyCsrfToken extends Middleware
      *
      * @var array<int, string>
      */
-    protected $except = [
-        'api/login',
-        'api/logout',
-        // 'api/posts',
-        'http://127.0.0.1:8000/api/posts/*',
-        'http://127.0.0.1:8000/api/register',
 
-        // 'posts/*/comments',
-        'http://127.0.0.1:8000/api/comments/*',
-        'http://127.0.0.1:8000/api/posts/*/comments',
+    protected $except = [
+        //
+
     ];
+
+    protected function inExceptArray($request)
+    {
+        $dbHost = env('DB_HOST');
+
+        $this->except = array($this->except, [
+
+            $dbHost . '/api/posts/*',
+            $dbHost . '/api/register',
+            $dbHost . '/api/comments/*',
+            $dbHost . '/api/posts/*/comments',
+            $dbHost . '/api/login',
+            $dbHost . '/api/logout'
+        ]);
+
+        return parent::inExceptArray($request);
+
+
+        // 'api/login',
+        // 'api/logout',
+        // 'http://127.0.0.1:8000/api/posts/*',
+        // 'http://127.0.0.1:8000/api/register',
+        // 'http://127.0.0.1:8000/api/comments/*',
+        // 'http://127.0.0.1:8000/api/posts/*/comments',
+
+    }
 
    
 }
